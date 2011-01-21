92bf099e6e1737bdbce185642b22999e44de43f7
Emma Tosch <etosch@gmail.com>
2011-01-20 19:15:20 -0500

    committed project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject HMM "1.0.1-SNAPSHOT"
+  :description "HMM implementation"
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :resources-path "resources"
+  :main core.main)
